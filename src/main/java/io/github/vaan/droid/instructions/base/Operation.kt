48618283724325c54a7de2c63b1cbdb89ecf0f44
Intybyte <vaan.testwork@gmail.com>
2026-03-18 20:53:47 +0100

    Operation shortcut

--- src/main/java/io/github/vaan/droid/instructions/base/Operation.kt
+++ src/main/java/io/github/vaan/droid/instructions/base/Operation.kt
@@ -1,11 +1,15 @@
 package io.github.vaan.droid.instructions.base
 
 import io.github.vaan.droid.PylonDroid
+import io.github.vaan.droid.data.DynamicDroidData
 
 data class Operation(
     val instruction: Instruction,
     val args: Array<String>
 ) {
+    fun execute(data: DynamicDroidData) {
+        instruction.execute(data, args)
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
