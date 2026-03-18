96e282bb84e4d461532f0ff7c5f584499067ce75
Intybyte <vaan.testwork@gmail.com>
2026-03-18 19:24:27 +0100

    Valued wrapper

---
+++ src/main/java/io/github/vaan/droid/data/Valued.kt
@@ -0,0 +1,76 @@
+package io.github.vaan.droid.data
+
+import org.bukkit.NamespacedKey
+import java.util.UUID
+
+interface Valued<T> {
+    val value: T
+    val type: Class<T>
+
+    interface CanBeString {
+        fun string(): String
+    }
+
+    interface IsNumber {
+        val number: Number
+    }
+
+    data class IntVal(override val value: Int) : Valued<Int>, IsNumber {
+        override val type = Int::class.java
+
+        override val number: Number = value
+    }
+
+    data class DoubleVal(override val value: Double) : Valued<Double>, IsNumber {
+        override val type = Double::class.java
+
+        override val number: Number = value
+    }
+
+    data class LongVal(override val value: Long) : Valued<Long>, IsNumber {
+        override val type = Long::class.java
+
+        override val number: Number = value
+    }
+
+    data class StringVal(override val value: String) : Valued<String>, CanBeString {
+        override val type = String::class.java
+
+        override fun string(): String = value
+    }
+
+    data class UUIDVal(override val value: UUID) : Valued<UUID>, CanBeString {
+        override val type = UUID::class.java
+
+        override fun string(): String = value.toString()
+    }
+
+    data class KeyVal(override val value: NamespacedKey) : Valued<NamespacedKey>, CanBeString {
+        override val type = NamespacedKey::class.java
+
+        override fun string(): String = value.toString()
+    }
+
+    companion object {
+        operator fun invoke(value: Any?): Valued<*>? = when (value) {
+            is Valued<*> -> value // already wrapped
+
+            is Int -> IntVal(value)
+            is Double -> DoubleVal(value)
+            is Float -> DoubleVal(value.toDouble())
+            is Long -> LongVal(value)
+            is Short -> IntVal(value.toInt())
+            is Byte -> IntVal(value.toInt())
+
+            is String -> StringVal(value)
+
+            is UUID -> UUIDVal(value)
+
+            is NamespacedKey -> KeyVal(value)
+
+            null -> null
+
+            else -> error("Didn't find valid Valued for ${value::class.java.name}")
+        }
+    }
+}
---
+++ src/main/java/io/github/vaan/droid/data/serializers/ValuedSerializer.kt
@@ -0,0 +1,99 @@
+package io.github.vaan.droid.data.serializers
+
+import io.github.pylonmc.rebar.datatypes.RebarSerializers
+import io.github.vaan.droid.PylonDroid
+import io.github.vaan.droid.data.Valued
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
+
+object ValuedSerializer : PersistentDataType<PersistentDataContainer, Valued<*>> {
+
+    private val typeKey = PylonDroid.key("type")
+    private val valueKey = PylonDroid.key("value")
+
+    override fun getPrimitiveType(): Class<PersistentDataContainer> = PersistentDataContainer::class.java
+
+    override fun getComplexType(): Class<Valued<*>> = Valued::class.java
+
+    override fun toPrimitive(
+        complex: Valued<*>,
+        context: PersistentDataAdapterContext
+    ): PersistentDataContainer {
+
+        val pdc = context.newPersistentDataContainer()
+
+        when (complex) {
+            is Valued.IntVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "int")
+                pdc.set(valueKey, RebarSerializers.INTEGER, complex.value)
+            }
+
+            is Valued.DoubleVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "double")
+                pdc.set(valueKey, RebarSerializers.DOUBLE, complex.value)
+            }
+
+            is Valued.LongVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "long")
+                pdc.set(valueKey, RebarSerializers.LONG, complex.value)
+            }
+
+            is Valued.StringVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "string")
+                pdc.set(valueKey, RebarSerializers.STRING, complex.value)
+            }
+
+            is Valued.UUIDVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "uuid")
+                pdc.set(valueKey, RebarSerializers.UUID, complex.value)
+            }
+
+            is Valued.KeyVal -> {
+                pdc.set(typeKey, RebarSerializers.STRING, "key")
+                pdc.set(valueKey, RebarSerializers.NAMESPACED_KEY, complex.value)
+            }
+
+            else -> error("Unsupported Valued type: ${complex::class.java.name}")
+        }
+
+        return pdc
+    }
+
+    override fun fromPrimitive(
+        primitive: PersistentDataContainer,
+        context: PersistentDataAdapterContext
+    ): Valued<*> {
+
+        val type = primitive.get(typeKey, RebarSerializers.STRING)
+            ?: error("Missing type in Valued")
+
+        return when (type) {
+            "int" -> Valued.IntVal(
+                primitive.get(valueKey, RebarSerializers.INTEGER)!!
+            )
+
+            "double" -> Valued.DoubleVal(
+                primitive.get(valueKey, RebarSerializers.DOUBLE)!!
+            )
+
+            "long" -> Valued.LongVal(
+                primitive.get(valueKey, RebarSerializers.LONG)!!
+            )
+
+            "string" -> Valued.StringVal(
+                primitive.get(valueKey, RebarSerializers.STRING)!!
+            )
+
+            "uuid" -> Valued.UUIDVal(
+                primitive.get(valueKey, RebarSerializers.UUID)!!
+            )
+
+            "key" -> Valued.KeyVal(
+                primitive.get(valueKey, RebarSerializers.NAMESPACED_KEY)!!
+            )
+
+            else -> error("Unknown Valued type: $type")
+        }
+    }
+}
